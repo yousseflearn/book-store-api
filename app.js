b7c856051bb0760e2e48495afa98225960d2b885
yousseflearn <youssefeasylearn@gmail.com>
2024-12-16 23:47:59 +0400

    Creation of .env file and Middleware logger

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const booksPath = require('./routes/books.js');
 const authorsPath = require('./routes/authors');
 const mongoose = require('mongoose');
+const logger = require('./middlewares/logger.js');
+const dotenv = require('dotenv');
+const { hostname } = require('os');
+dotenv.config();
 
 // Connection To Database
 mongoose
-  .connect('mongodb://localhost/bookStoreDB')
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('Connected To MongoDB...'))
   .catch((error) => console.log('Connection Failed To Mongodb', error));
 
@@ -16,13 +20,14 @@ const app = express();
 /* The express.json() function is a built-in middleware in Express that is used for parsing incoming requests with JSON payload. The express.json middleware is important for parsing incoming JSON payloads and making that data available in the req.body or further processing within the routes. Without using express.json, Express will not automatically parse the JSON data in the request body.
 By using the express.json middleware, you can handle POST, PUT, or PATCH requests that send JSON data from the client to the server.*/
 app.use(express.json());
+app.use(logger);
 
 //routes
 app.use('/api/books', booksPath);
 app.use('/api/authors', authorsPath);
 
 // Running The Server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
